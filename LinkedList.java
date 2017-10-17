1e40e0dc890d9caa4e7eb399b8e55a15eb9dfc72
ChrisLink <lyh.link1998@gmail.com>
2017-10-17 04:03:14 -1100

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,64 @@
+/**
+ * An Iterable list that is implemented using a singly-linked chain of nodes
+ * with a header node and without a tail reference.
+ * 
+ * The "header node" is a node without a data reference that will 
+ * reference the first node with data once data has been added to list.
+ * 
+ * The iterator returned is a LinkedListIterator constructed by passing 
+ * the first node with data.
+ * 
+ * CAUTION: the chain of nodes in this class can be changed without
+ * calling the add and remove methods in this class.  So, the size()
+ * method must be implemented by counting nodes.  This counting must
+ * occur each time the size method is called.  DO NOT USE a numItems field.
+ * 
+ * COMPLETE THIS CLASS AND HAND IN THIS FILE
+ */
+public class LinkedList<E> implements ListADT<E> {
+
+
+	//	 TODO: YOU MUST IMPLEMENT THE LINKED LIST CLASS AS FOLLOWS:
+	//	 
+	//	 It must be a SINGLY-LINKED chain of ListNode<E> nodes
+	//	 It must have a "header node" ("dummy node" without data)
+	//	 It must NOT have a tail reference
+	//	 It must NOT keep a number of data items
+	//       NOTE: in this program, the chains of nodes in this program may be 
+	//   	 changed outside of the LinkedList class, so the actual data count 
+	//       must be determined each time size is called.
+	//
+	//	 It must return a LinkedListIterator<E> as its iterator.
+	//	 
+	//	 Note: The "header node"'s data reference is always null and 
+	//	 its next references the node with the first data of the list.
+	//	 
+	//	 Be sure to implement this LinkedList<E> using Listnode
+	//	       you must use LinkedListIterator<E> instead of Iterator<E>
+	//	
+
+	/** 
+	 * Returns a reference to the header node for this linked list.
+	 * The header node is the first node in the chain and it does not 
+	 * contain a data reference.  It does contain a reference to the 
+	 * first node with data (next node in the chain). That node will exist 
+	 * and contain a data reference if any data has been added to the list.
+	 * 
+	 * NOTE: Typically, a LinkedList would not provide direct access
+	 * to the headerNode in this way to classes that use the linked list.  
+	 * We are providing direct access to support testing and to 
+	 * allow multiple nodes to be moved as a chain.
+	 * 
+	 * @return a reference to the header node of this list. 0
+	 */
+	public Listnode<E> getHeaderNode() {
+		//TODO implement this method
+	}
+
+	/**
+	 * Must return a reference to a LinkedListIterator for this list.
+	 */
+	public LinkedListIterator<E> iterator() {
+		//TODO implement this method
+	}
+}
